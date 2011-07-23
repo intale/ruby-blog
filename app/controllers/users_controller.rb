1a14a7d54c39cb07bc1d1d8e973f7b41e5e22f18
Ivan Dzyzenko <intale.a@gmail.com>
2011-07-23 17:06:01 +0300

    deploying home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,7 @@
 class UsersController < ApplicationController
 
-  before_filter :search_logic
-
   def index
-    @posts = @search.paginate(:per_page => 10, :page => params[:page])
-  end
-
-  private
-
-  def search_logic
-    defaults = {'meta_sort' => 'created_at.desc'}
-    @search = Post.search(defaults.merge(params[:search] || {}))
+    @posts = Post.includes(:admin).paginate(:per_page => 5, :page => params[:page])
   end
 
 end
