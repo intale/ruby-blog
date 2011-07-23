5feb2403eb17a33798981a8575a8cc30c410fabd
Ivan Dzyzenko <intale.a@gmail.com>
2011-07-23 17:31:48 +0300

    comments table fixes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110723125355) do
+ActiveRecord::Schema.define(:version => 20110723143019) do
 
   create_table "admins", :force => true do |t|
     t.string   "nick"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(:version => 20110723125355) do
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "post_id"
   end
 
   create_table "posts", :force => true do |t|
