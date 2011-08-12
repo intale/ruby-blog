40643c20ebd8770f108cf6ab40de3b4bc1427514
Ivan Dzyzenko <intale.a@gmail.com>
2011-08-12 10:52:28 +0300

    added show method for administration::posts controller

--- app/controllers/administration/posts_controller.rb
+++ app/controllers/administration/posts_controller.rb
@@ -8,4 +8,14 @@ class Administration::PostsController < Administration::MainController
     @post = Post.includes(:comments).find(params[:id])
   end
 
+  def update
+    @post = Post.find(params[:id])
+    @post.update_attributes(params[:post])
+    redirect_to administration_post_path
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
 end
