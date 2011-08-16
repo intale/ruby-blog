949ee4c7cdb0e1393128e93b8b48f8df6c1892c0
Ivan Dzyzenko <intale.a@gmail.com>
2011-08-16 12:29:56 +0300

    html length fixes

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,7 +2,7 @@ module UsersHelper
 
   def truncate_html(html)
     result=''
-    Nokogiri::HTML.fragment(html).children.each {|elem| result+=elem.to_html if result.length <10}
+    Nokogiri::HTML.fragment(html).children.each {|elem| result+=elem.to_html if result.length <100}
     result
   end
 
