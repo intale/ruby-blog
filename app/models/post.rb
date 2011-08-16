f955ed6ddb070be4c50bcb5a23f135169eb96db7
Ivan Dzyzenko <intale.a@gmail.com>
2011-08-16 13:19:11 +0300

    added validation for Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,11 @@
 class Post < ActiveRecord::Base
+  attr_protected :admin_id
+
   belongs_to :admin
   has_many :comments, :dependent => :destroy
   has_and_belongs_to_many :tags
+
+  validates :admin_id, :subject, :message, :presence => true
 
   before_save :update_post_message
 
