d02bdc900b6ee7dee9ee4745a50bd810adbe61a3
Ivan Dzyzenko <intale.a@gmail.com>
2011-08-16 11:35:48 +0300

    added html's scope while showing and saving posts

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+
+  def truncate_html(html)
+    result=''
+    Nokogiri::HTML.fragment(html).children.each {|elem| result+=elem.to_html if result.length <10}
+    result
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,11 @@ class Post < ActiveRecord::Base
   belongs_to :admin
   has_many :comments, :dependent => :destroy
   has_and_belongs_to_many :tags
+
+  before_save :update_post_message
+
+  def update_post_message
+    self.message = Nokogiri::HTML.parse(self.message).search("body").inner_html
+  end
+
 end
