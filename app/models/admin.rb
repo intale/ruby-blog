2f60520bbde13ad47d98c971485e9f6d4c9bce45
Ivan Dzyzenko <intale.a@gmail.com>
2011-08-16 13:00:36 +0300

    added validation of Admin

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,10 +1,22 @@
 class Admin < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :rememberable, :trackable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :username
 
   has_many :posts
+  validates :password, :confirmation => true, :if => :password_required?
+  validates :nick, :presence => true
+  validates :username, :email,
+            :presence => true,
+            :length => {:minimum => 3, :maximum => 20},
+            :uniqueness => { :case_sensitive => false }
+  validates :email, :format => {:with => Devise.email_regexp}
+
+  protected
+
+  def password_required?
+    new_record? || !password.nil? || !password_confirmation.nil?
+  end
+
 end
