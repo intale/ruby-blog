6adc00e0cad6f592f5dead3a8c319e09dcddf9b0
Ivan Dzyzenko <intale.a@gmail.com>
2011-08-16 13:10:04 +0300

    added validation for Comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,8 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
+
+  validates :content, :post_id, :author, :presence => true
+  validates :content, :length => {:minimum => 1, :maximum => 10000}
+  validates :author, :length => {:minimum => 2, :maximum => 20}
+
 end
