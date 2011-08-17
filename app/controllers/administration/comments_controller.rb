30f87880b96cb4221da80349596ac58d74fa4061
Ivan Dzyzenko <intale.a@gmail.com>
2011-08-17 11:33:17 +0300

    updated administration::comments controller

--- app/controllers/administration/comments_controller.rb
+++ app/controllers/administration/comments_controller.rb
@@ -1,15 +1,15 @@
 class Administration::CommentsController < Administration::MainController
 
-  def edit
-    @comment = Comment.includes(:post).find(params[:id])
-  end
+  before_filter :find_comment, :except => :index
 
   def index
     @comments = Comment.includes(:post).paginate(:per_page => 50, :page => params[:page])
   end
 
+  def edit
+  end
+
   def update
-    @comment = Comment.includes(:post).find(params[:id])
     if @comment.update_attributes(params[:comment])
       flash[:notice] = "Comment updated successfully"
       redirect_to administration_post_path(@comment.post)
@@ -20,10 +20,15 @@ class Administration::CommentsController < Administration::MainController
   end
 
   def destroy
-    @comment=Comment.find(params[:id])
     @comment.destroy
     flash[:notice] = "Comment deleted successfully"
     redirect_to administration_post_path(@comment.post)
+  end
+
+  private
+
+  def find_comment
+    @comment = Comment.includes(:post).find(params[:id])
   end
 
 end
