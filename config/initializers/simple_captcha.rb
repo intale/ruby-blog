1dcdb7c916a5d8497b9a4b5fbe065f2888931737
Ivan Dzyzenko <intale.a@gmail.com>
2011-08-19 18:04:33 +0300

    added captcha's mixing mode; changed captcha's image

--- config/initializers/simple_captcha.rb
+++ config/initializers/simple_captcha.rb
@@ -16,9 +16,35 @@ SimpleCaptcha.setup do |sc|
   # 'charcoal_grey',
   # 'almost_invisible'
   # 'random'
-  sc.image_style = 'charcoal_grey'
+  sc.image_style = 'ruby_blog'
 
   # default: low
   # possible values: 'low', 'medium', 'high', 'random'
   sc.distortion = 'medium'
+end
+
+module SimpleCaptcha #:nodoc
+  module ImageHelpers
+    @@image_styles = {'ruby_blog' => ['-shade 80x65', '-background "#e8e8e8"']}
+  end
+end
+module SimpleCaptcha #:nodoc
+  module ViewHelper #:nodoc
+
+    private
+    def generate_simple_captcha_data(code)
+      value = ''
+
+      case code
+        when 'numeric'.to_sym then
+          SimpleCaptcha.length.times { value << (48 + rand(10)).chr }
+        when 'mixing'.to_sym then
+          SimpleCaptcha.length.times { value << [(48 + rand(10)),(65 + rand(26))][rand(2)].chr }
+        else
+          SimpleCaptcha.length.times { value << (65 + rand(26)).chr }
+      end
+
+      value
+    end
+  end
 end
