e2f8884c69e5a1bf07a21f75d58784a00fc63b84
Ivan Dzyzenko <intale.a@gmail.com>
2011-08-31 18:00:28 +0300

    admin can set comment's author....

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -14,7 +14,6 @@ class Comment < ActiveRecord::Base
   def check_author
     nick = Admin.select(:nick).find_by_nick(self.author.strip.downcase)
     errors.add(:author, "Nick #{self.author} has already been taken by admin") if nick && !self.current_admin
-    errors.add(:author, "You are not #{self.author}. Don't even try to fuck me!") if self.current_admin and self.current_admin.nick != self.author
   end
 
 end
