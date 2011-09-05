71ba838fc3d3fcbbcb12459050474c755ff6ce42
Ivan Dzyzenko <intale.a@gmail.com>
2011-09-05 11:11:01 +0300

    small fixes

--- app/controllers/administration/tags_controller.rb
+++ app/controllers/administration/tags_controller.rb
@@ -1,25 +1,24 @@
 class Administration::TagsController < Administration::MainController
 
+    before_filter :find_post, :only => [:create, :destroy]
+
   def index
     @tags = Tag.all.paginate(:per_page => 20, :page => params[:page])
   end
 
   def create
     tag = Tag.find_or_create_by_name(params[:tag][:name])
-    post = Post.find_by_id(params[:tag][:post_id])
-    unless !post and post.tags.exists?(tag)
-      post.tags << tag
-      result = {:tag => render_to_string(:partial => 'tag', :locals =>{:tag => tag, :post => post})}
+    unless @post.nil? or @post.tags.exists?(tag)
+      @post.tags << tag
+      result = {:tag => render_to_string(:partial => 'tag', :locals =>{:tag => tag, :post => @post})}
     end
-    result ||= {:error => "Association between tag_id #{tag.id} and post_id #{post.id} already exist"}
+    result ||= {:error => "Association between tag_id #{tag.id} and post_id #{params[:post_id]} already exist or post not found"}
     render :json => result
-
   end
 
   def destroy
     tag = Tag.find_by_id(params[:id])
-    post = Post.find_by_id(params[:post_id])
-    if tag and post and post.tags.delete(tag)
+    if tag and @post and @post.tags.delete(tag)
       render :text => "ok"
     else
       render :text => "Error while deleting association between tag_id #{params[:id]} and post_id #{params[:post_id]}"
@@ -35,6 +34,12 @@ class Administration::TagsController < Administration::MainController
       flash[:error] = "Could not find tag with id #{params[:id]}"
     end
     redirect_to administration_tags_path
+  end
+
+  private
+
+  def find_post
+    @post = Post.find_by_id(params[:post_id])
   end
 
 end
