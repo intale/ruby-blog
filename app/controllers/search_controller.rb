327dfd897a846526c46fe0dd96101607fe88e9b4
Jenua Boiko <jeyboy@bigmir.net>
2011-09-05 13:11:36 +0300

    Complete search by comments and posts

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,12 @@
 class SearchController < ApplicationController
   def index
-    search_str=params[:search][:req].gsub('%','\%').gsub('_','\_').downcase
-    @posts = Post.where("lower(message) like '%#{search_str}%' OR lower(subject) like '%#{search_str}%'").all
-    @comments = Comment.where("lower(content) like '%#{search_str}%'").all
+    @search_str=params[:search][:req].gsub('%','\%').gsub('_','\_').downcase
+    @is_posts=params[:search][:category]=="in posts"
+    if @is_posts
+      @find = Post.where("lower(message) like '%#{@search_str}%' OR lower(subject) like '%#{@search_str}%'").paginate(:per_page => 20, :page => params[:page])
+    else
+      @find = Comment.where("lower(content) like '%#{@search_str}%'").paginate(:per_page => 20, :page => params[:page])
+    end
 
     #@posts = Post.includes(:admin).order('created_at desc').paginate(:per_page => 5, :page => params[:page])
   end
