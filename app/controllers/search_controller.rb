dbde1909ea4826b1633d0a93a133c008cb2651e0
Jenua Boiko <jeyboy@bigmir.net>
2011-09-05 17:33:18 +0300

    Add meta_search gem and logic for him

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,13 +1,10 @@
 class SearchController < ApplicationController
   def index
-    @search_str=params[:search][:req].gsub('%','\%').gsub('_','\_').downcase
     @is_posts=params[:search][:category]=="in posts"
     if @is_posts
-      @find = Post.where("lower(message) like '%#{@search_str}%' OR lower(subject) like '%#{@search_str}%'").paginate(:per_page => 20, :page => params[:page])
+      @find = Post.search(:message_or_subject_contains=>params[:search][:request]).paginate(:per_page => 20, :page => params[:page])
     else
-      @find = Comment.where("lower(content) like '%#{@search_str}%'").paginate(:per_page => 20, :page => params[:page])
+      @find = Comment.search(:content_contains=>params[:search][:request]).paginate(:per_page => 20, :page => params[:page])
     end
-
-    #@posts = Post.includes(:admin).order('created_at desc').paginate(:per_page => 5, :page => params[:page])
   end
 end
