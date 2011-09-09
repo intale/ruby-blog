734f5a7524489a1d91fe74aebb4f9bd6dca52acd
Ivan Dzyzenko <intale.a@gmail.com>
2011-09-09 17:25:15 +0300

    code optimization

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,7 @@
 class TagsController < ApplicationController
 
   def index
-    @posts = Post.includes(:tags).where('tags.name = ?', params[:name]).paginate(:per_page => 5, :page => params[:page])
+    @posts = Post.includes(:tags, :admin).where('tags.name = ?', params[:name]).paginate(:per_page => 5, :page => params[:page])
     render :template => "users/index"
   end
 
