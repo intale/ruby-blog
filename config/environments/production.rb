fdbc838b4b4cda384c0330e0bdb554d18a8e5b34
Ivan Dzyzenko <intale.a@gmail.com>
2011-09-21 13:37:16 +0300

    enable caching in production mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ RubyBlog::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
-  config.cache_classes = false
+  config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
@@ -26,7 +26,7 @@ RubyBlog::Application.configure do
   # config.force_ssl = true
 
   # See everything in the log (default is :info)
-  # config.log_level = :debug
+  config.log_level = :info
 
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
