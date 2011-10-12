287d068fe0311c6c03bff9d0c8bbacb79395c1b0
Ivan Dzyzenko <intale.a@gmail.com>
2011-10-12 17:59:46 +0300

    rus_alpha module fixes

--- lib/rus_alpha.rb
+++ lib/rus_alpha.rb
@@ -2,12 +2,22 @@
 
 module RusAlpha
   RUS_ALPHAS = {"а" => "a", "б" => "b", "в" => "v", "г" => "g", "д" => "d", "е" => "e", "ё" => "e", "ж" => "j",
-                "з" => "z", "и" => "i", "к" => "k", "л" => "l", "м" => "m", "н" => "n", "о" => "o", "п" => "p",
+                "з" => "z", "и" => "i", "й" => "y" ,"к" => "k", "л" => "l", "м" => "m", "н" => "n", "о" => "o", "п" => "p",
                 "р" => "r", "с" => "s", "т" => "t", "у" => "u", "ф" => "f", "х" => "h", "ц" => "ts", "ч" => "ch",
-                "ш" => "sh", "щ" => "sch", "ь" => "'", "ъ" => "'", "э" => "e", "ю" => "yu", "я" => "ya"}
+                "ш" => "sh", "щ" => "sch", "э" => "e", "ю" => "yu", "я" => "ya", "ь" => "", "ъ" => ""}
 
   def self.translate(string)
-    string.gsub(/./){|s| RUS_ALPHAS.has_key?(s) ? RUS_ALPHAS[s] : s}
+    string.gsub(/./){|s| s = downcase_rus(s); RUS_ALPHAS.has_key?(s) ? RUS_ALPHAS[s] : s}
+  end
+
+  private
+
+  def self.downcase_rus(rus_symbol)
+    if rus_symbol =~ /[А-Я|Ё]/
+      (rus_symbol == "Ё" ? rus_symbol.ord + 80 : rus_symbol.ord + 32).chr
+    else
+      rus_symbol
+    end
   end
 
 end
