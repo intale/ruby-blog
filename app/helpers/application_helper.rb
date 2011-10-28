4b452ca42e900fcc8fe401ab68d1f1addb248b02
Ivan Dzyzenko <intale.a@gmail.com>
2011-10-28 15:46:48 +0300

    fixes meta keywords for users#index and tags#index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,11 +10,11 @@ module ApplicationHelper
           page_number = $3 || 1
         end
         { :title => "Категория: #{tag_name}, страница #{page_number}", :keywords => tag_name.to_s, :description => "Все темы по категории #{tag_name}" }
-      when /^\/\?page=(\d+)$/
-        request.fullpath.scan(/^\/\?page=(\d+)$/) do
-          page_number = $1
+      when /^\/(\?page=)?(\d+)?$/
+        request.fullpath.scan(/^\/(\?page=)?(\d+)?$/) do
+          page_number = $2 || 1
         end
-        { :title => "RubyBlog, страница #{page_number}", :keywords => @tags.map(&:name).join(" "), :description => "Блог, созданый и посвященный Ruby on Rails." }
+        { :title => "RubyBlog, страница #{page_number}", :keywords => @posts.map{|p| p.tags.map(&:name)}.flatten.uniq.join(" "), :description => "Блог, созданый и посвященный Ruby on Rails." }
       when /\/post\/\d+/
         { :title => @post.subject, :keywords => @post.tags.map(&:name).join(" "), :description => "#{@post.subject}. #{Nokogiri::HTML.fragment(truncate_html(@post)).content}" }
       else
