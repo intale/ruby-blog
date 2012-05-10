92626abe05a9dbed501cfb65e70041db959b8969
sedzen <sedzen@sedzen.(none)>
2012-05-10 14:47:38 +0300

    finish feature_186.

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,9 +1,8 @@
 class AdminMailer < ActionMailer::Base
   default from: "noreply@is-valid.org"
 
-  def comment_notifier(comment,post)
+  def comment_notifier(comment)
     @admins = Admin.select(:email).subscribed.all.map(&:email)
-    @post = Post.find_by_id(post)
     @comment = Comment.find_by_id(comment)
     mail(:to => @admins, :subject => "#{@comment.author} left a comment")
   end
