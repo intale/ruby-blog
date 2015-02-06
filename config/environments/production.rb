f7eb4d633170e9c3268654a1532c980084876451
Ivan Dzyzenko <ivan.dzyzenko@gmail.com>
2015-02-06 16:34:23 +0200

    fix prod env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ RubyBlog::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_files = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -50,6 +50,7 @@ RubyBlog::Application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
+  config.assets.compile = false
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners
