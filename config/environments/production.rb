b3ddea5e177336c2b2e23da1f384c32075740b16
Ivan Dzyzenko <ivan.dzyzenko@gmail.com>
2015-02-06 17:23:36 +0200

    enable assets digests

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,6 +42,9 @@ RubyBlog::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( administration/admins.css administration/admins.js )
 
+  # Generate digests for assets URLs
+  config.assets.digest = true
+
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
